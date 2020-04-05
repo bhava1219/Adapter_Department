1e0a8e51e5a4fef7f14eabbdb12829d69176835c
Bhavadhaarany <bhavadhaarany12@gmail.com>
2020-04-05 18:10:47 +0530

    Initial commit

---
+++ app/src/main/java/com/bhava/adapter_department/Item.java
@@ -0,0 +1,25 @@
+package com.bhava.adapter_department;
+public class Item {
+
+    private String itemName;
+    private String itemDescription;
+    private String itemPhonenumber;
+
+    public Item(String name, String description , String ph) {
+        this.itemName = name;
+        this.itemDescription = description;
+        this.itemPhonenumber = ph;
+    }
+
+    public String getItemName() {
+        return this.itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public String getitemPhonenumber(){
+        return this.itemPhonenumber;
+    }
+}
